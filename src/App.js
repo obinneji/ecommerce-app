29c97f78e24a130dd4b6675d97666c6567d949b2
obinneji <obinnnejic@gmail.com>
2023-01-21 06:26:16 +0100

    creation of different routes in app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import AuthPage from "./components/AuthPage";
+import Cart from "./components/Cart";
+import Collection from "./components/Collections";
+import Contact from "./components/Contact";
+import About from "./components/About"
+
 
 
 function App() {
@@ -8,11 +15,12 @@ function App() {
     <>
     <Header />
     <Routes>
-      {/* <Route path="/" element={<Dashboard /> } /> */}
-      {/* <Route path="contact" element={<Contact />} /> */}
-      {/* <Route path="about" element={<About />} /> */}
-      {/* <Route path="collections" element={<Collections />} /> */}
-      {/* <Route path='cart' element={<Cart />} /> */}
+      <Route path="/"  element={<Dashboard />} />
+      <Route path="collections" element={<Collection />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path='login' element={<AuthPage />} />
     </Routes>
     <Footer />
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,42 @@
 
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+// import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
+import { Link } from 'react-router-dom';
 const Header = () => {
+
   return (
     <div className='bg-blue'>
-      <nav className=' text-white px-7 py-10 font-poppins flex items-center justify-between'>
+      <nav className=' text-white px-7 py-10 font-poppins flex lg:items-center justify-between lg:flex-row flex-col item-left'>
         {/* <img className='' alt='brand-logo' src=''></img> */}
-        <h1 className=' ml-10 text-4xl text-white font-bold'>BUYNOW</h1>
-        <ul className='flex gap-16 mr-20'>
-          <li><a href=' ' className='font-medium text-xl'>Collections</a></li>
-          <li><a href='' className='font-medium text-xl'>About</a></li>
-          <li><a href='' className='font-medium text-xl'>Contact</a></li>
+        <h1 className=' ml-4 text-4xl text-white font-bold'>BUYNOW</h1>
+        <ul className='flex lg:gap-16 mr-20 flex-col lg:flex-row gap-6 mt-5 mb-5'>
+          <li><Link to='/collections' className='font-medium text-xl'>Collections</Link></li>
+          <li><Link to='/about' className='font-medium text-xl'>About</Link></li>
+          <li><Link to='/contact' className='font-medium text-xl'>Contact</Link></li>
         </ul>
-        <form className='w-50 flex relative'>
-          <input type='text' className='w-96 p-2 rounded-full mx-2 text-black outline-1' placeholder='search for your favourite'/>
-          <div className='bg-yellow rounded-full w-10 h-10 absolute inset-y-0 right-0 mr-2 flex items-center justify-center cursor-pointer'>
-          <SearchOutlinedIcon className=''/>
+        <form className="flex items-center w-96">
+          <label for="simple-search" className="sr-only">Search</label>
+          <div className="relative w-full">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+            </div>
+            <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for any product" required />
           </div>
+          <button type="submit" className="p-2.5 ml-2 text-sm font-medium text-white bg-yellow rounded-full border border-yellow hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+            <span className="sr-only">Search</span>
+          </button>
         </form>
-        <div className='flex align-center cursor-pointer'>
+        <div className='flex align-center cursor-pointet mt-5 mb-5 '>
           <PersonOutlineIcon />
-          <a href='' className='text-xl '>Sign in Acct</a>
+          <Link to='/login' className='text-xl '>Sign in Acct</Link>
         </div>
         <div className='flex align-center cursor-pointer'>
-          <p className='text-xl '><AddShoppingCartOutlinedIcon className='text-2xl'/>(0)</p>
+          <Link to='/cart'>
+          <p className='text-xl '><AddShoppingCartOutlinedIcon className='text-2xl' />(0)</p>
+
+          </Link>
         </div>
       </nav>
     </div>
